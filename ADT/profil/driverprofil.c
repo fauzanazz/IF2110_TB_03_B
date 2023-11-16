f1eba86ea27abc44fe2607bb59900d975e6cc5ed
LastPrism7 <67941358+LastPrism7@users.noreply.github.com>
2023-11-16 10:40:18 +0700

    Add files via upload

---
+++ ADT/profil/driverprofil.c
@@ -0,0 +1,28 @@
+#include "profil.h"
+#include "profil.c"
+#include <stdio.h>
+#include "../pengguna/pengguna.h"
+#include "../pengguna/pengguna.c"
+#include "../Pcolor/pcolor.h"
+#include "../Pcolor/pcolor.c"
+#include "../matrixprofil/matrixprofil.h"
+#include "../matrixprofil/matrixprofil.c"
+#include "../Mesin-Kata/charmachine.h"
+#include "../Mesin-Kata/charmachine.c"
+#include "../Mesin-Kata/wordmachine.h"
+#include "../Mesin-Kata/wordmachine.c"
+#include "../boolean.h"
+
+int main(){
+    ListPengguna lp;
+    createListPengguna(&lp);
+    Daftar(&lp);
+    Masuk(&lp);
+    gantiProfil(&(lp).user[0]);
+    aturJenisAkun(&(lp).user[0]);
+    lihatProfil((lp).user[0]);
+    aturJenisAkun(&(lp).user[0]);
+    lihatProfil((lp).user[0]);
+    UBAH_FOTO_PROFIL(&(lp).user[0]);
+    lihatProfil((lp).user[0]);
+}
