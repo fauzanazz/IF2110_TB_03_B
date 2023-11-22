f5b22ce11fb936e5fab78d809f34e1eb5402a519
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2023-11-22 10:26:24 +0700

    Docs: matrix profil

--- ADT/matrixprofil/matrixprofil.h
+++ ADT/matrixprofil/matrixprofil.h
@@ -7,10 +7,32 @@ typedef struct {
 
 #define PIXEL(F, i, j) (F).pix[(i)][(j)]
 
+/**
+ * Membuat foto profil baru.
+ * 
+ * Prosedur ini digunakan untuk membuat foto profil baru dengan menginisialisasi
+ * semua atribut pada objek Foto.
+ * 
+ * @param F Pointer ke objek Foto yang akan dibuat.
+ */
 void createFotoProfil(Foto *F);
 
+/**
+ * Menampilkan foto profil.
+ * 
+ * Prosedur ini digunakan untuk menampilkan foto profil yang telah dibuat.
+ * 
+ * @param F Objek Foto yang akan ditampilkan.
+ */
 void displayFotoProfil(Foto F);
 
+/**
+ * Mengganti foto profil.
+ * 
+ * Prosedur ini digunakan untuk mengganti foto profil dengan foto baru.
+ * 
+ * @param F Pointer ke objek Foto yang akan diubah.
+ */
 void gantiFotoProfil(Foto *F);
 
 #endif
